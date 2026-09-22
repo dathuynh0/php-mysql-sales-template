d4a96db6f2653f4dcfd67882f5c87b65996eaf6a
dathuynh0 <hcdattv@gmail.com>
2026-09-22 07:43:07 +0700

    Khởi tạo PHP Docker cho dự án

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+$appName = "Hệ thống quản lý bán hàng";
+?>
+
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= $appName ?></title>
+</head>
+<body>
+
+    <h1><?= $appName ?></h1>
+    <p>Ứng dụng PHP đang hoạt động.</p>
+</body>
+</html>
